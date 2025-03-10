4256c724cca9f1680f52d61219a71ef0ebeb2d08
valennns <valenciasutio31@gmail.com>
2025-03-10 11:05:14 +0700

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -47,5 +47,9 @@ def update_tab(tab_name):
         ])
 
 # Jalankan server
+import os
+
 if __name__ == '__main__':
-    app.run_server(debug=False, threaded=True)
+    port = int(os.environ.get('PORT', 8050))
+    app.run_server(host='0.0.0.0', port=port, debug=False)
+
